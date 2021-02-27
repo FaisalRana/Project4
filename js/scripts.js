262ee3214169d71a0d4ab8c613fb2259fadb6bff
Faisal Rana <inquisitive@gmail.com>
2021-02-27 13:22:07 -0800

    commit working page to gh-pages

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ let veggieT = [];
 let cheese = "cheese";
 let price = 0;
 let size = "";
+let numMeats = 0;
+let numVeggies = 0;
 
 function Pizza(size, meatT, veggieT) {
   this.size = size;
@@ -15,10 +17,24 @@ function Pizza(size, meatT, veggieT) {
 
 let pizza1 = new Pizza(size, meatT, veggieT);
 
-Pizza.prototype.addTops = function() {
+Pizza.prototype.addSize = function() {
    let inputSize = $("#question1 option:selected").val()
    this.size = size + inputSize;
+   if (inputSize === "Large") {
+     this.price = this.price + 14;
+   } else if(inputSize === "Medium") {
+     this.price = this.price + 10;
+   } else {
+     this.price = this.price + 5;
+   }
 }
+
+Pizza.prototype.modifyPrice = function() {
+  numMeats = numMeats + meatT.length;
+  numVeggies = numVeggies + veggieT.length;
+  this.price = this.price + (numMeats * 2) + (numVeggies * 1)
+}
+
 
 //user logic
 
@@ -34,9 +50,14 @@ $(document).ready(function() {
       const veggies = $(this).val();
       veggieT.push(veggies);
     });
-    let pizza1 = new Pizza(size, meatT, veggieT);
-    pizza1.addTops();
     $("#hiddenOutput").show();
+    pizza1.addSize();
+    pizza1.modifyPrice();
+    $("#sizeP").append($("#question1 option:selected").val());
+    $("#toppingsP").append(meatT.join("<br>"));
+    $("#toppingsP").append("<br>");
+    $("#toppingsP").append(veggieT.join("<br>"));
+    $("#priceP").append(pizza1.price)
   });
 });
 

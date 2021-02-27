47d015ad935ee472f52e739f9db76e641b43f157
Faisal Rana <inquisitive@gmail.com>
2021-02-26 18:25:59 -0800

    used jquery selectors to link form output to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,36 @@
+// //business logic 
 
-function Pizza(pSize, cheese, meatT, veggieT) {
-  this.pSize = pizzaSize;
-  this.cheese = cheese;
-  this.meatT = [];
-  this.veggieT = [];
-}
+// function Pizza(pSize, cheese, meatT, veggieT) {
+//   this.pSize = pizzaSize;
+//   this.cheese = cheese;
+//   this.meatT = [];
+//   this.veggieT = [];
+// }
 
-pizzaSize = 
+// let pizzaSize = "";
 
-$("input:checkbox[name=meat-topping]:checked").each(function() {
-  const meat = $(this).val();
-  meatT.push(meat);
+// $("input:checkbox[name=meat-topping]:checked").each(function() {
+//   const meat = $(this).val();
+//   meatT.push(meat);
+// });
+
+//user logic
+
+  
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    let arr = [];
+    $("#sizeO").append($("#question1 option:selected").val());
+    $("input:checkbox[name=meatT]:checked").each(function() {
+      const meats = $(this).val();
+      $("#toppingsO").append(meats + "<br>");
+    });
+    $("input:checkbox[name=veggieT]:checked").each(function() {
+      const veggies = $(this).val();
+      $("#toppingsO").append(veggies + "<br>");
+    });
+    $("#hiddenOutput").show();
+  });
 });
+
